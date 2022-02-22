0bfa739e889ad3769cf0a233e2e268391bdcfd3f
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-02-22 12:24:57 +0300

    The function delayedGreet console logs 'welcome' after 3 seconds

--- async exercises/index.js
+++ async exercises/index.js
@@ -13,3 +13,13 @@ testMe()
 
 
 // Partnah is first printed to the console, and when its over, sayHowdy is pushed into the call stack and called to log Howdy to the console.
+
+
+
+// Challenge 2
+
+function delayedGreet() { 
+  console.log("welcome")
+}
+
+setTimeout(delayedGreet, 3000)
