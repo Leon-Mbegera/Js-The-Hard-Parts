a7403cdae219cc83cef2f71bf1cee1be3bf68038
Leon Mbegera <mbegeraleon89@gmail.com>
2022-10-13 12:18:40 +0300

    Asynchronous functions, print the string 'Hello' after 1000ms.

---
+++ promises/index.js
@@ -0,0 +1,12 @@
+// Challenge 1
+
+function sayHello() {
+  function printHello() {
+    console.log("Hello!");
+  }
+  setTimeout(printHello, 1000)
+}
+
+  
+// Uncomment the line below when ready
+sayHello(); // should log "Hello" after 1000ms
