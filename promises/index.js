d2c5968e64ebe960c826b44a1f81e804dcc0684c
Leon Mbegera <mbegeraleon89@gmail.com>
2022-10-13 15:57:04 +0300

    use Promise.all to make 3 API calls and return all the data when the calls are complete

--- promises/index.js
+++ promises/index.js
@@ -72,3 +72,31 @@ let firstPromise = new Promise(function(resolve, reject) {
 
 
 firstPromise.then((value) => console.log(value));
+
+
+
+const fakePeople = [
+  { name: 'Rudolph', hasPets: false, currentTemp: 98.6 },
+  { name: 'Zebulon', hasPets: true, currentTemp: 22.6 },
+  { name: 'Harold', hasPets: true, currentTemp: 98.3 },
+]
+
+const fakeAPICall = (i) => {
+  const returnTime = Math.floor(Math.random() * 1000);
+  return new Promise((resolve, reject) => {
+    if (i >= 0 && i < fakePeople.length) {
+      setTimeout(() => resolve(fakePeople[i]), returnTime);
+    } else {
+      reject({ message: "index out of range" });
+    }
+  });
+};
+
+function getAllData() {
+  // CODE GOES HERE
+  Promise.all([fakeAPICall(0), fakeAPICall(1), fakeAPICall(2)]).then((arrOfObjs) => {
+    console.log(arrOfObjs);
+  })
+}
+
+getAllData();
