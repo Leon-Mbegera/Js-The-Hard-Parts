bf55f2c3aad8b38381c07c7f46b2eff89e14c559
Leon Mbegera <mbegeraleon89@gmail.com>
2022-10-18 11:47:36 +0300

    Create a functional iterator for an array that returns each value of the array when called, one element at a time.

--- iterators/iterators.js
+++ iterators/iterators.js
@@ -11,3 +11,24 @@ function sumFunc(arr) {
 }
 
 // console.log(sumFunc([1, 2, 3]));
+
+// B) Create a functional iterator for an array that returns each value of the array when called, one element at a time.
+
+function returnIterator(arr) {
+    // YOUR CODE HERE
+    let i = 0;
+    function myIterator() {
+        const current = arr[i];
+        i++;
+        return current;
+    }
+    return myIterator;
+}
+
+// Uncomment the lines below to test your work
+const array2 = ['a', 'b', 'c', 'd'];
+const myIterator = returnIterator(array2);
+console.log(myIterator()); // -> should log 'a'
+console.log(myIterator()); // -> should log 'b'
+console.log(myIterator()); // -> should log 'c'
+console.log(myIterator()); // -> should log 'd'
