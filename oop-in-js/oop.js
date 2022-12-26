d9d8463e9dd8ffad1d92a190a1912535644683b8
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-12-26 15:50:37 +0300

    DeveloperClass challenge

--- oop-in-js/oop.js
+++ oop-in-js/oop.js
@@ -288,3 +288,39 @@ const george = new PersonClass('George');
 
 // Uncomment this line to check your work!
 // george.greet(); // -> Logs 'hello'
+
+
+
+
+// Challenge: DeveloperClass
+// Create a class DeveloperClass that creates objects by extending the PersonClass class. In addition to having a name property and greet method, DeveloperClass should have an introduce method. When called, introduce should log the string "Hello World, my name is [name]".
+
+
+class PersonClass {
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet() {
+        console.log('hello');
+    }
+}
+
+// add code here
+
+class DeveloperClass extends PersonClass {
+    constructor(name, age) {
+        super(name);
+        this.age = age;
+    }
+
+    introduce() {
+        console.log(`Hello World, my name is ${this.name}`)
+    }
+}
+
+const thai = new DeveloperClass('Thai', 32);
+
+// Uncomment these lines to check your work!
+// console.log(thai.name); // -> Logs 'Thai'
+// thai.introduce(); // -> Logs 'Hello World, my name is Thai'
