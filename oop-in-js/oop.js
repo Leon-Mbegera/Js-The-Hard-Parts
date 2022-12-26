2ff0ba867a73fc28261004997bd5bbcaa895604b
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-12-26 15:39:00 +0300

    PersonClass challenge

--- oop-in-js/oop.js
+++ oop-in-js/oop.js
@@ -216,47 +216,75 @@ const fido = new Dog('Fido', 'poodle');
 
 function Inventory(item, price) {
     // add code 
-    Object.assign(this, {[item]: {
-      price: price,
-      quantity: 1
-    }})
-  }
-  
-  Inventory.prototype.addItem = function(item, price) {
+    Object.assign(this, {
+        [item]: {
+            price: price,
+            quantity: 1
+        }
+    })
+}
+
+Inventory.prototype.addItem = function (item, price) {
     if (this[item]) {
-      this[item].quantity++;
-      this[item].price = price;
+        this[item].quantity++;
+        this[item].price = price;
     } else {
-        Object.assign(this, {[item]: {
-        price: price,
-        quantity: 1
-        }})
+        Object.assign(this, {
+            [item]: {
+                price: price,
+                quantity: 1
+            }
+        })
     }
-  }
-  
-  Inventory.prototype.deleteItem = function(item) {
+}
+
+Inventory.prototype.deleteItem = function (item) {
     if (this[item]) {
-      if (this[item].quantity > 0) {
-        this[item].quantity--;
-          return "Deleted"
-          } else return "Nothing to delete"	
-      } 
-  }
-  
-  Inventory.prototype.checkItem = function(exists) {
-    if(this[exists]) {
-      return this[exists];
-      } else return "Item is not in inventory"
-  }
-  
-  const myInventory = new Inventory('cucumber', 2);
-  
+        if (this[item].quantity > 0) {
+            this[item].quantity--;
+            return "Deleted"
+        } else return "Nothing to delete"
+    }
+}
+
+Inventory.prototype.checkItem = function (exists) {
+    if (this[exists]) {
+        return this[exists];
+    } else return "Item is not in inventory"
+}
+
+const myInventory = new Inventory('cucumber', 2);
+
 // Uncomment these lines to check your work!
 //   myInventory.addItem('carrot', 1);
-  
+
 //   console.log(myInventory.checkItem('cucumber')); // Logs: { price: 2, quantity: 1 }
 //   myInventory.addItem('cucumber', 3); 
 //   console.log(myInventory.deleteItem('carrot')); // Logs: 'Deleted'
 //   console.log(myInventory.deleteItem('carrot')); // Logs: 'Nothing to delete'
 //   console.log(myInventory); // Logs: { cucumber: { price: 3, quantity: 2 }, carrot: { price: 1, quantity: 0 } }
 //   console.log(myInventory.checkItem('radish')); // Logs: 'Item is not in inventory'
+
+
+// Challenge: PersonClass
+// Create a class PersonClass. PersonClass should have a constructor that is passed an input of name and saves it to a property by the same name. PersonClass should also have a method called greet that logs the string "hello".
+
+
+class PersonClass {
+    constructor(name) {
+        // add code here
+        this.name = name;
+    }
+
+    greet() {
+        console.log("hello")
+    }
+
+    // add code here
+
+}
+
+const george = new PersonClass('George');
+
+// Uncomment this line to check your work!
+// george.greet(); // -> Logs 'hello'
