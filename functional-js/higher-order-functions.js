af580f04f5c057c72fd255eaeeb1dccb598a0ca2
Leon-Mbegera <mbegeraleon89@gmail.com>
2023-01-05 15:52:34 +0300

    High order function with a callback

--- functional-js/higher-order-functions.js
+++ functional-js/higher-order-functions.js
@@ -21,3 +21,23 @@ function addS(input) {
 // Uncomment these to check your work
 // console.log(addS('pizza'));
 // console.log(addS('bagel'));
+
+
+
+// Challenge 3
+// Create a function called map that takes two inputs:
+// 1. an array of numbers (a list of numbers)
+// 2. a 'callback' function - a function that is applied to each element of the array (inside of the         function 'map')
+// Have map return a new array filled with numbers that are the result of using the 'callback' function on each element of the input array.
+
+const map = (array, callback) => {
+    const outputArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        outputArray.push(callback(array[i]))
+    }
+
+    return outputArray;
+};
+
+console.log(map([1, 2, 3], addTwo));
