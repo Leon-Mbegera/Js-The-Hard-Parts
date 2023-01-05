ff5def8678cc77cd3846f1f6cfdbb031d010ef52
Leon-Mbegera <mbegeraleon89@gmail.com>
2023-01-05 15:41:01 +0300

    a function addS that accepts one input and adds an s to it

--- functional-js/higher-order-functions.js
+++ functional-js/higher-order-functions.js
@@ -6,5 +6,18 @@ function addTwo(input) {
 }
 
 // To check if you've completed this function, uncomment these console.logs!
-console.log(addTwo(3));
-console.log(addTwo(10));
+// console.log(addTwo(3));
+// console.log(addTwo(10));
+
+
+
+// Challenge 2
+// Create a function addS that accepts one input and adds an "s" to it.
+
+function addS(input) {
+    return input + "s"
+}
+
+// Uncomment these to check your work
+// console.log(addS('pizza'));
+// console.log(addS('bagel'));
