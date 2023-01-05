938be014773b48ede0e6382ca849e2acf6d66099
Leon-Mbegera <mbegeraleon89@gmail.com>
2023-01-05 15:37:55 +0300

    a function addTwo that accepts one input and adds 2 to it

---
+++ functional-js/higher-order-functions.js
@@ -0,0 +1,10 @@
+// Challenge 1
+// Create a function addTwo that accepts one input and adds 2 to it.
+
+function addTwo(input) {
+    return input + 2
+}
+
+// To check if you've completed this function, uncomment these console.logs!
+console.log(addTwo(3));
+console.log(addTwo(10));
