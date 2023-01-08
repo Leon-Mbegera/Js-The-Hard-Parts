2e6e2ed185a3cb8a317b76b9bd7db1952c136cfc
Leon-Mbegera <mbegeraleon89@gmail.com>
2023-01-08 10:50:28 +0300

    Rebuild key/value pairs of an object from the matching elements of two input arrays

--- functional-js/higher-order-functions.js
+++ functional-js/higher-order-functions.js
@@ -137,8 +137,31 @@ const union = (arrays) => {
 
 };
 
-console.log(union([[5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]]));
+// console.log(union([[5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]]));
 // should log: [5, 10, 15, 88, 1, 7, 100]
+
+
+
+// Challenge 9
+// Construct a function objOfMatches that accepts two arrays and a callback. objOfMatches will build an object and return it. To build the object, objOfMatches will test each element of the first array using the callback to see if the output matches the corresponding element (by index) of the second array. If there is a match, the element from the first array becomes a key in an object, and the element from the second array becomes the corresponding value.
+
+
+const objOfMatches = (array1, array2, callback) => {
+
+    const myObj = {};
+
+    for (let i = 0; i < array1.length; i++) {
+        if (callback(array1[i]) === array2[i]) {
+            myObj[array1[i]] = array2[i];
+        }
+    }
+
+    return myObj;
+
+};
+
+// console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy', 'BYE', 'LATER', 'hello'], (str) => str.toUpperCase()));
+// should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
 
 
 
