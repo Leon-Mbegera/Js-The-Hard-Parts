1b56cd260be79b171fdb0fabdbd70d0e46de3f31
Leon-Mbegera <mbegeraleon89@gmail.com>
2023-01-08 12:21:54 +0300

    Looping through an object using the for..in technique loops through even the properties in the objects prototype chain

--- functional-js/higher-order-functions.js
+++ functional-js/higher-order-functions.js
@@ -201,16 +201,52 @@ const commutative = (func1, func2, value) => {
 };
 
 // /*** Uncomment these to check your work! ***/
-const multBy3 = n => n * 3;
-const divBy4 = n => n / 4;
-const subtract5 = n => n - 5;
-console.log(commutative(multBy3, divBy4, 11)); // should log: true
-console.log(commutative(multBy3, subtract5, 10)); // should log: false
-console.log(commutative(divBy4, subtract5, 48)); // should log: false
+// const multBy3 = n => n * 3;
+// const divBy4 = n => n / 4;
+// const subtract5 = n => n - 5;
+// console.log(commutative(multBy3, divBy4, 11)); // should log: true
+// console.log(commutative(multBy3, subtract5, 10)); // should log: false
+// console.log(commutative(divBy4, subtract5, 48)); // should log: false
 
 
 
+// Challenge 12
+// Create a function objFilter that accepts an object and a callback. objFilter should make a new object, and then iterate through the passed-in object, using each key as input for the callback. If the output from the callback is equal to the corresponding value, then that key-value pair is copied into the new object. objFilter will return this new object.
 
+
+const objFilter = (obj, callback) => {
+
+    const newFilterObj = {};
+
+};
+
+// /*** Uncomment these to check your work! ***/
+// const startingObj = {};
+// startingObj[6] = 3;
+// startingObj[2] = 1;
+// startingObj[12] = 4;
+// const half = n => n / 2;
+// console.log(objFilter(startingObj, half)); // should log: { 2: 1, 6: 3 }
+
+
+function Construct(name, email) {
+
+    this.name = name;
+    this.email = email;
+
+}
+
+Construct.prototype.add = 1;
+
+
+const user = new Construct("John Doe", "John.doe@example.com")
+
+// iterate over the user object
+
+for (const key in user) {
+
+    // console.log(`${key}: ${user[key]}`);
+}
 
 
 
