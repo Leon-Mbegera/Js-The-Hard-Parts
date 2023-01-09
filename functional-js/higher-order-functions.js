b5d6cd5342b70698da3f049a104bba413bff6117
Leon-Mbegera <mbegeraleon89@gmail.com>
2023-01-09 10:55:30 +0300

    Passing one function's output as an input for the next function

--- functional-js/higher-order-functions.js
+++ functional-js/higher-order-functions.js
@@ -295,6 +295,29 @@ const checks = [isEven, greaterThanFour, isSquare, hasSix];
 
 
 
+// Challenge 14
+// Create a function pipe that accepts an array (of functions) and a value. pipe should input the value into the first function in the array, and then use the output from that function as input for the second function, and then use the output from that function as input for the third function, and so forth, until we have an output from the last function in the array. pipe should return the final output.
+
+
+
+const pipe = (arrOfFuncs, value) => {
+
+    let output = value;
+
+    arrOfFuncs.forEach((func) => {
+        output = func(output)
+    })
+    return output;
+};
+
+// /*** Uncomment these to check your work! ***/
+const capitalize = str => str.toUpperCase();
+const addLowerCase = str => str + str.toLowerCase();
+const repeat = str => str + str;
+const capAddlowRepeat = [capitalize, addLowerCase, repeat];
+console.log(pipe(capAddlowRepeat, 'cat')); // should log: 'CATcatCATcat'
+
+
 
 
 
